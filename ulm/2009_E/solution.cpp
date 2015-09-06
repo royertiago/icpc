06f6298c62ff9a36e25894e1d2bad0fdf38d3cf0
Tiago Royer <royertiago@gmail.com>
2015-09-06 16:37:15 -0300

    Simplify ulm/2009_E/solution.cpp.

--- ulm/2009_E/solution.cpp
+++ ulm/2009_E/solution.cpp
@@ -3,87 +3,87 @@
 #define INF (1 << 30)
 int min( int i, int j ) { return i < j ? i : j; }
 
-int c[129];
+int c[128];
 
 // s[i][j] = c[i] + c[i+1] + ... + c[j]
-int s[129][129];
+int s[128][128];
 
-/* dp[i][j][k] is the cheapest way of coding the numbers c[1],...,c[i],
- * such that exactly j different prefixes are used,
- * and forcing the last prefix to cover the last k digit count
- * (that is, the last prefix must represent all numbers
- * with i-k+1, i-k+2, ..., i-1 and i digits).
+/* dp[i][j] is the least amount of bits needed to encode
+ * all numbers with size between 1 and i+1
+ * using at exactly j+1 different prefix sizes.
+ * (The "exactly" part will reduce the search effort.)
  *
- * Note we always have j, k <= i.
- * Note also j + k >= i + 1:
- * as the last group have k elements, the remaining i-k
- * must form at most j-1 groups. Thus, i-k <= j-1,
- * or i+1 <= j+k.
- * So we have the lower bound i+1-j for k:
- *   i+1-j <= k <= i.
+ * Note we will always have j <= i.
  */
-int dp[129][129][129];
-
+int dp[128][128];
 int n;
 
 int solve() {
     // Precompute s[i][j]
-    for( int i = 1; i <= n; i++ ) {
+    for( int i = 0; i < n; i++ ) {
         s[i][i] = c[i];
-        for( int j = i + 1; j <= n; j++ )
+        for( int j = i + 1; j < n; j++ )
             s[i][j] = s[i][j-1] + c[j];
     }
 
-    // To simplify the solution, set to INF impossible values.
-    for( int i = 0; i <= n; i++ )
-    for( int j = 0; j <= n; j++ )
-    for( int k = 0; k <= n; k++ )
-        dp[i][j][k] = INF;
+    /* Base case: i == 0.
+     * The only way of encoding all numbers
+     * is to use 1 bit to the prefix and 1 bit to encode the number,
+     * thus resulting in 2 bits per number.
+     * As we have c[0] different numbers to encode,
+     *  2*c[0]
+     * is the optimum value.
+     */
+    dp[0][0] = 2*c[0];
 
-    // Base case: i == 1.
-    dp[1][1][1] = 2*c[1];
+    // Induction: i > 0.
+    for( int i = 1; i < n; i++ ) {
+        /* "Sub-base" case: j == 0,
+         * so everything have the same unary prefix
+         * (remember dp[i][j] is the optimum with j+1 different prefixes).
+         *
+         * We have s[0][i] different numbers,
+         * and each will need to be encoded with i+1 bits,
+         * plus one bit for the prefix.
+         */
+        dp[i][0] = s[0][i] * (i+1 + 1);
 
-    // Induction
-    for( int i = 2; i <= n; i++ ) {
+        // Induction: j > 0.
         for( int j = 1; j <= i; j++ ) {
-            /* Two subcases. First, when k == 1,
-             * we must allocate a new prefix for the numbers with i digits,
-             * so we will brute-force through all possible values of k
-             * to minimize dp[i-1][j-1].
+            /* For each k >= 1, we will try to create a new block
+             * with the last k number sizes
+             * and assign a single prefix to them.
+             * The prefix has size j+1,
+             * and the numbers have to be coded with i+1 bits.
              *
-             * The value, for each case, is dp[i-1][j-1][k]
-             * plus the cost of encoding each of the c[i] numbers
-             * which have size i, using a prefix of size j.
+             * The last k numbers are i-(k-1), i-(k-2), ..., i-(k-k),
+             * so we have s[i-k+1][i] numbers in this new block;
+             * the other i-k+1 number sizes
+             * have minimum cost dp[i-k][j-1].
+             *
+             * This last observation imposes the upper bound on k:
+             * as we must have j-1 <= i-k, we also must have
+             *  k <= i+1-j.
              */
-            dp[i][j][1] = INF;
-            for( int k = 1; k <= i; k++ )
-                dp[i][j][1] = min( dp[i][j][1],
-                                   dp[i-1][j-1][k] + c[i]*(i+j) );
-            /* Now, when k > 1.
-             * To compute dp[i][j][k],
-             * we are forced to build upon dp[i-1][j][k-1],
-             * because we will not create another group.
-             * The value is then dp[i-1][j][k-1]
-             * plus the cost of encoding the c[i] i-sized numbers
-             * using a prefix of size j,
-             * plus the padding of the numbers in the previous group,
-             * which amounts to one extra digit per number.
-             */
-            for( int k = 2; k <= i; k++ )
-                dp[i][j][k] = dp[i-1][j][k-1] +
-                    s[i-k+1][i-1] + c[i]*(i + j);
+            dp[i][j] = INF;
+            for( int k = 1; k <= i+1-j; k++ )
+                dp[i][j] = min( dp[i][j],
+                                dp[i-k][j-1] + s[i-k+1][i] * (i+1 + j+1) );
         }
     }
+
+    /* Then, just minimize the value of dp[n-1][j]
+     * for every possible number of prefix sizes j.
+     */
     int ans = INF;
-    for( int j = 1; j <= n; j++ )
-    for( int k = 1; k <= n; k++ )
-        ans = min( ans, dp[n][j][k] );
+    for( int j = 0; j < n; j++ )
+        ans = min( ans, dp[n-1][j] );
     return ans;
 }
 
 int main() {
     while( scanf("%d", &n) != EOF && n != 0 ) {
-        for( int i = 1; i <= n; i++ )
+        for( int i = 0; i < n; i++ )
             scanf( "%d", &c[i] );
         printf( "%d\n", solve() );
     }
